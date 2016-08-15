8139725b43c23b57072578483f3429ae47b18729
Adrien Pyke <adpyke@gmail.com>
2016-08-14 22:22:43 -0400

    fixed it ya goober

--- hummingbird-compatibility-hover/hummingbird-compatibility-hover.user.js
+++ hummingbird-compatibility-hover/hummingbird-compatibility-hover.user.js
@@ -17,7 +17,7 @@ var DELAY_SEC = 0.75;
 var CACHED = {};
 
 var Util = {
-  log: function() {
+  log: function () {
     var args = [].slice.call(arguments);
     args.unshift('%c' + SCRIPT_NAME + ':', 'font-weight: bold;color: #233c7b;');
     console.log.apply(console, args);
@@ -42,10 +42,10 @@ var Util = {
   addStyle: function(styles) {
     var sel, css, obj;
     var out = '';
-    for (sel in styles) {
+    for(sel in styles) {
       obj = styles[sel];
       out += sel + '{';
-      for (css in obj) {
+      for(css in obj) {
         out += css + ':' + obj[css] + ';';
       }
       out += '}';
@@ -61,7 +61,7 @@ var hb = {
 };
 
 var App = {
-  userLinkRegex: /hummingbird\.me\/users\/.+/,
+  userLinkRegex: /^https?:\/\/(forums\.)?hummingbird\.me\/users\/[^\/]+\/?(\?.*)?$/,
 
   styleUI: function() {
     var style = {
@@ -108,7 +108,7 @@ var App = {
   getCompatArea: function() {
     var compatArea = Util.q('#' + COMPAT_ID);
 
-    if (!compatArea) {
+    if(!compatArea) {
       compatArea = document.createElement('div');
       compatArea.id = COMPAT_ID;
       compatArea.style.display = 'none';
@@ -123,17 +123,16 @@ var App = {
   },
 
   showCompat: function(me, them) {
-    if (me === them) return;
-    if (Util.q('.signup-cta')) return; // not signed in
+    if(me === them) return;
+    if(Util.q('.signup-cta')) return; // not signed in
 
     var self = this;
     var area = this.getCompatArea();
 
     var cache = CACHED[me + '+' + them];
-    if (cache) {
+    if(cache) {
       area.innerHTML = self.getCompatHTML(cache);
-    }
-    else {
+    } else {
       area.innerHTML = '<img src="' + LOAD_GIF + '" />';
       Util.getJSON('https://hbird-cmp-node.herokuapp.com/compatibility/anime?user1=' + me + '&user2=' + them, function(compat) {
         CACHED[me + '+' + them] = compat;
@@ -158,7 +157,7 @@ var App = {
   startHover: function(e) {
     App._timeout = setTimeout(function() {
       App._timeout = null;
-      App.showCompat(hb.getUserName(Util.q('.dropdown-menu > li > a').href), hb.getUserName(e.target.href));
+      App.showCompat(hb.getUserName(Util.q('.dropdown-menu > li > a, #current-user > a').href), hb.getUserName(e.target.href));
     }, DELAY_SEC * 1000);
   },
   stopHover: function(e) {
@@ -171,9 +170,10 @@ var App = {
     Util.log('starting...');
     self.styleUI();
     waitForElems('a', function(link) {
-      if (self.userLinkRegex.test(link.href)) {
+      if(self.userLinkRegex.test(link.href)) {
         link.addEventListener('mouseenter', self.startHover);
         link.addEventListener('mouseleave', self.stopHover);
+        link.addEventListener('click', self.stopHover);
       }
     });
   }
