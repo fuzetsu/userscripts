7b79f8ee889f9248d336de40f7232b9b14b48892
synthtech <synthtech0@gmail.com>
2017-02-08 00:18:12 -0500

    Re:Start

--- kitsu-mal-rating/kitsu-mal-rating.user.js
+++ kitsu-mal-rating/kitsu-mal-rating.user.js
@@ -2,96 +2,33 @@
 // @name         Kitsu MAL Rating
 // @namespace    http://fuzetsu.com/kitsu-mal-rating
 // @version      2.0
-// @description  Shows MyAnimeList.net rating in kitsu.io
+// @description  Shows MyAnimeList.net rating on Kitsu.io
 // @author       fuzetsu
 // @match        *://kitsu.io/*
-// @match        http://myanimelist.net/404.php?*
-// @grant        none
-// @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=141779
+// @match        *://myanimelist.net/*
+// @require      https://greasyfork.org/scripts/5679-wait-for-elements/code/Wait%20For%20Elements.js?version=147465
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-var url = window.location.href;
-if (/^http:\/\/myanimelist\.net\/404\.php\?.*/.test(url)) {
-  console.log('MAL embedded anime page, showing score...');
-  document.body.innerHTML = '<style>.highlight {color: #16a085; font-weight: 400;} body {color: rgb(115, 115, 115);font-family: klavika, Helvetica, Arial, sans-serif;font-size: 16px;font-weight: normal;</style>';
-  var query = {};
-  url.slice(url.indexOf('?') + 1).split('&').forEach(function(q) {
-    var p = q.split('=');
-    query[p[0]] = unescape(p[1]);
-  });
-  displayMALScore(query.title, query.media, document.body);
-}
-else {
-  if (window.top !== window.self) return;
-  console.log('Hummingbird page, waiting for anime page to embed score');
-  var frame;
-  waitForUrl(/^http(s)?:\/\/hummingbird.me\/anime\/.*/, function() {
-    if (frame) frame.remove();
-    waitForElems('.hb-score', function(elem) {
-      var title = document.querySelector('.series-title').childNodes[2].textContent.trim().replace(/\([^\)]*\)/g, ""),
-        media = document.querySelector('.series-metadata > li:nth-child(1)').textContent.trim();
-      frame = elem.appendChild(document.createElement('iframe'));
-      frame.src = 'http://myanimelist.net/404.php?title=' + escape(title) + '&media=' + media;
-      frame.setAttribute('style', 'max-width: 100%;border: none;height: 60px;');
-    }, true);
-  });
-}
+(function() {
+  'use strict';
 
-function requestMAL(url, cb) {
-  var xhr = new XMLHttpRequest();
-  if (url.indexOf('myanimelist') === -1) {
-    url = 'http://myanimelist.net/' + url;
-  }
-  xhr.open('get', url);
-  xhr.responseType = 'document';
-  xhr.onload = function() {
-    cb(xhr.response);
+  var SCRIPT_NAME = 'Kitsu MAL Rating';
+  var API = 'https://kitsu.io/api/edge';
+  var REGEX = /^https?:\/\/kitsu\.io\/(anime|manga)\/([^\/]+)\/?(?:\?.*)?$/;
+
+  var Util = {
+    log: function() {
+      var args = [].slice.call(arguments);
+      args.unshift('%c' + SCRIPT_NAME + ':', 'font-weight: bold;color: #233c7b;');
+      console.log.apply(console, args);
+    },
+    q: function(query, context) {
+      return (context || document).querySelector(query);
+    },
+    qq: function(query, context) {
+      return [].slice.call((context || document).querySelectorAll(query));
+    }
   };
-  xhr.send();
-}
 
-function searchMAL(query, media, cb) {
-  requestMAL('anime.php?q=' + escape(query), function(res) {
-    var results = [];
-    var resultRows = res.querySelectorAll('#content > div:nth-child(2) > table > tbody > tr');
-    if (resultRows.length === 0) { // if the are no result rows then we got redirected, pass page and exit early
-      cb({
-        page: res,
-        direct: true
-      });
-      return;
-    }
-    [].slice.call(resultRows, 1).forEach(function(anime) {
-      var a = anime.querySelector('td:nth-child(2) > a:nth-child(2)'),
-        m = anime.querySelector('td:nth-child(3)').textContent.trim();
-      if (a) results.push({
-        title: a.textContent.trim(),
-        media: m,
-        url: a.href
-      });
-    });
-    cb(results);
-  });
-}
-
-function displayMALScore(title, media, place) {
-  searchMAL(title, media, function(results) {
-    function showScore(page) {
-      place.innerHTML += 'MAL ' + page.querySelectorAll('#content > table > tbody > tr > td.borderClass > h2')[2].nextSibling.textContent.replace(/[0-9]*\.[0-9]*/, '<span class="highlight">$&</span>');
-    }
-    console.log(title, media, results); // TESTING
-    if (results.direct) {
-      showScore(results.page);
-    }
-    else {
-      var regSpecial = /[^A-z0-9]/g;
-      title = title.replace(regSpecial, '');
-      results.some(function(anime) {
-        if (anime.title.replace(regSpecial, '').indexOf(title) !== -1 && anime.media === media) {
-          requestMAL(anime.url, showScore);
-          return true;
-        }
-      });
-    }
-  });
-}
+})();
