b3e2c88e0fc08de1730e79a02c031e47d11ec9cd
synthtech <synthtech0@gmail.com>
2017-02-08 01:36:04 -0500

    Find MAL id

--- kitsu-mal-rating/kitsu-mal-rating.user.js
+++ kitsu-mal-rating/kitsu-mal-rating.user.js
@@ -31,4 +31,49 @@
     }
   };
 
+  var App = {
+    getMalLink: function(type, slug, cb) {
+      Util.log('Fetching mappings for Kitsu slug:', type + '/' + slug);
+      GM_xmlhttpRequest({
+        method: 'GET',
+        url: API + '/' + type + '?filter[slug]=' + slug + '&fields[' + type + ']=id&include=mappings',
+        headers: {
+          'Accept': 'application/vnd.api+json'
+        },
+        onload: function(response) {
+          try {
+            var json = JSON.parse(response.responseText);
+            Util.log('Kitsu ' + type + ' ID:', json.data[0].id);
+            var malId;
+            for (var i = 0; i < json.included.length; i++) {
+              if (json.included[i].attributes.externalSite == ('myanimelist/' + type)) {
+                malId = json.included[i].attributes.externalId;
+              }
+            }
+            cb(malId);
+          } catch (err) {
+            Util.log('Failed to parse API results');
+          }
+        },
+        onerror: function() {
+          Util.log('Failed to get Kitsu mappings');
+        }
+      });
+    }
+  };
+
+  waitForUrl(REGEX, function() {
+    var type = location.href.match(REGEX)[1];
+    var slug = location.href.match(REGEX)[2];
+    Util.log('Parsed URL type:', type);
+    Util.log('Parsed URL slug:', slug);
+
+    App.getMalLink(type, slug, function(malId) {
+      if (!malId) {
+        Util.log('MAL ID not found');
+      } else {
+        Util.log('Received MAL ID:', malId);
+      }
+    });
+  });
 })();
