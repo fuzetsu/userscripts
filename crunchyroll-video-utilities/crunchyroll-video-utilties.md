5bb66dcf45593c2b452f440314963648551e70e0
fuzetsu <fuzetsu490@gmail.com>
2018-11-08 23:44:51 -0500

    crunchyroll-video-utilities v1.0.1: - moved from crunchyroll-default-quality - added keybindings for   * skipping between episodes   * skipping segements of time   * changing volume - added readme

---
+++ crunchyroll-video-utilities/crunchyroll-video-utilties.md
@@ -0,0 +1,11 @@
+# Crunchyroll Video Utilities
+
+Enables the following functionality on Crunchyroll's video player:
+
+- **Set default video quality:** configure through script menu
+- **Keybindings:**
+  - `p/n - previous/next episode`
+  - `j/k - volume down/up`
+  - `[/] - 15 seconds backwards/forwards`
+  - `{/} - 30 seconds backwards/forwards`
+  - `b/l - 90 seconds backwards/forwards` _(the length of a standard opening/ending)_
---
+++ crunchyroll-video-utilities/crunchyroll-video-utilties.user.js
@@ -0,0 +1,131 @@
+// ==UserScript==
+// @name Crunchyroll Video Utilities
+// @version 1.0.1
+// @namespace fuzetsu/csdvqn
+// @match https://static.crunchyroll.com/vilos/player.html
+// @match https://www.crunchyroll.com/*
+// @grant GM_registerMenuCommand
+// @grant GM_getValue
+// @grant GM_setValue
+// @require https://raw.githubusercontent.com/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
+// @require https://raw.githubusercontent.com/kufii/My-UserScripts/master/libs/gm_config.js
+// ==/UserScript==
+
+const sep = '~~@~~'
+const domain = 'https://www.crunchyroll.com'
+const qualitySelector = '.qualityMenuItemSelector'
+const settingsSelector = '.settingsMenuButton'
+
+// use publicly exposed Player.js object to control video player
+const vilosPlayer = () => unsafeWindow.VILOS_PLAYERJS
+
+const qq = (q, c) => Array.from((c || document).querySelectorAll(q))
+const q = (q, c) => (c || document).querySelector(q)
+
+const config = GM_config([
+  {
+    key: 'quality',
+    label: 'Quality',
+    type: 'dropdown',
+    values: ['auto', 360, 480, 720, 1080],
+    default: 'auto'
+  }
+])
+
+let cfg = config.load()
+
+config.onsave = newCfg => {
+  cfg = newCfg
+  player.setQuality(cfg.quality)
+}
+
+const p = (...args) => (console.log(...args), args[0])
+const prevent = (e, fn) => (e.preventDefault(), fn(e))
+
+const player = {
+  setQuality: quality => {
+    const btn =
+      quality !== 'auto'
+        ? qq(qualitySelector)
+            .slice(2)
+            .find(item => quality >= parseInt(item.textContent))
+        : qq(qualitySelector)[2]
+    if (btn) {
+      // this causes the menu to open
+      btn.click()
+      // so close it after a short delay
+      setTimeout(player.toggleSettings, 200, false)
+    }
+  },
+  fullscreen: () => q('.vjs-fullscreen-control').click(),
+  nextEp: (back = false) =>
+    q('.collection-carousel-media-link-current')
+      .parentElement[back ? 'previousElementSibling' : 'nextElementSibling'].querySelector('a')
+      .click(),
+  prevEp: () => player.nextEp(true),
+  skip: sec => vilosPlayer().getCurrentTime(curTime => vilosPlayer().setCurrentTime(curTime + sec)),
+  volumeUp: (val = 10) => vilosPlayer().getVolume(curVol => vilosPlayer().setVolume(curVol + val)),
+  volumeDown: (val = -10) => player.volumeUp(val),
+  toggleSettings: makeVisible => {
+    const btn = q(settingsSelector)
+    if (makeVisible) {
+      const isVisible = p(!!btn.offSetParent, '=== isVisible')
+      if (typeof makeVisible === 'boolean' && makeVisible === isVisible) return
+    }
+    btn.click()
+  }
+}
+
+const seekKeys = {
+  l: 90,
+  b: -90,
+  '}': 30,
+  '{': -30,
+  ']': 15,
+  '[': -15
+}
+
+const handleKey = key =>
+  key === 'n'
+    ? player.nextEp()
+    : key === 'p'
+    ? player.prevEp()
+    : key === 'j'
+    ? player.volumeDown()
+    : key === 'k'
+    ? player.volumeUp()
+    : key in seekKeys
+    ? player.skip(seekKeys[key])
+    : null
+
+const isPlayerFrame = location.href.includes('https://static.crunchyroll.com/vilos/player.html')
+
+/**
+ * Keyboard events from within the player frame dont bubble up to main page where the player.js
+ * object and the video links live, so use postMessage to send the keypresses there
+ **/
+window.addEventListener(
+  'keydown',
+  isPlayerFrame ? e => window.parent.postMessage(sep + e.key, domain) : e => handleKey(e.key)
+)
+
+if (isPlayerFrame) {
+  // can only set quality from the player frame since the button is in its dom
+  waitForElems({
+    stop: true,
+    sel: qualitySelector + '.selected',
+    onmatch: elem => {
+      if (elem.textContent.toLowerCase().includes(cfg.quality))
+        return console.log('configured default already selected')
+      player.setQuality(cfg.quality)
+    }
+  })
+} else {
+  // handle forwarded keyboard from player frame
+  window.addEventListener(
+    'message',
+    ({ data }) => data.startsWith(sep) && handleKey(data.slice(sep.length))
+  )
+}
+
+GM_registerMenuCommand('Crunchyroll Video Utilities - Config', config.setup)
