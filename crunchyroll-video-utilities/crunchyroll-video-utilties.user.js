8e93e59fb0d4839ad581fd7deddabf64659d46da
fuzetsu <fuzetsu490@gmail.com>
2018-11-17 22:47:34 -0500

    crvu: update require urls to comply - with greasyfork

--- crunchyroll-video-utilities/crunchyroll-video-utilties.user.js
+++ crunchyroll-video-utilities/crunchyroll-video-utilties.user.js
@@ -7,8 +7,8 @@
 // @grant GM_registerMenuCommand
 // @grant GM_getValue
 // @grant GM_setValue
-// @require https://raw.githubusercontent.com/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
-// @require https://raw.githubusercontent.com/kufii/My-UserScripts/master/libs/gm_config.js
+// @require https://gitcdn.xyz/cdn/fuzetsu/userscripts/b38eabf72c20fa3cf7da84ecd2cefe0d4a2116be/wait-for-elements/wait-for-elements.js
+// @require https://gitcdn.xyz/cdn/kufii/My-UserScripts/fa4555701cf5a22eae44f06d9848df6966788fa8/libs/gm_config.js
 // ==/UserScript==
 
 const sep = '~~@~~'
