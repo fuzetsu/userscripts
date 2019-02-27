5eaf32059ca1188fb7b9041469addbdef4e7f714
fuzetsu <fuzetsu490@gmail.com>
2019-02-26 20:23:46 -0500

    format-json 0.0.2: - don't set json with innerhtml since it might contain html...

--- format-json/format-json.user.js
+++ format-json/format-json.user.js
@@ -2,11 +2,13 @@
 // @name         Format JSON
 // @namespace    format-json
 // @author       fuzetsu
-// @version      0.0.1
+// @version      0.0.2
 // @description  Automatically prettify JSON responses
 // @match        *://*/*.json
 // @grant        GM_setClipboard
 // ==/UserScript==
 const json = JSON.parse(document.body.textContent)
 const formatted = JSON.stringify(json, null, 2)
-document.body.innerHTML = `<code><pre style="white-space:pre-wrap;word-break:break-word">${formatted}</pre></code>`
+document.body.innerHTML =
+  '<code><pre style="white-space:pre-wrap;word-break:break-word" id="jsonArea"></pre></code>'
+document.getElementById('jsonArea').textContent = formatted
