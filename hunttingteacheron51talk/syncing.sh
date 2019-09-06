cc73284095aa2a6699031dde728d3956d09ed6de
68681395 <happycoder@foxmail.com>
2019-09-06 15:57:38 +0800

    修改提交的批处理脚本

--- hunttingteacheron51talk/syncing.sh
+++ hunttingteacheron51talk/syncing.sh
@@ -1,6 +1,9 @@
 echo -n "输入提交介绍，默认为‘auto commit’ ->"
 read msg
- 
+ if [ -z "$msg" ]; then
+     echo "msg is set as 'auto commit'"
+	 msg='auto commit'
+ fi
 git pull
 git add *
 git commit -m $msg 
