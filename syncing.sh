ebb15f6acdfe1f543ff9990898c6821567b885f3
68681395 <happycoder@foxmail.com>
2019-09-06 16:16:16 +0800

    auto commit

--- syncing.sh
+++ syncing.sh
@@ -2,11 +2,11 @@ echo -n "输入提交介绍，默认为‘auto commit’ ->"
 read msg
  if [ -z "$msg" ]; then
      echo "committing as comment 'auto commit'"
-	 msg="'auto commit'"
+	 msg="auto commit"
  fi
 git pull
 git add *
-git commit -m $msg 
+git commit -m "$msg"
 git push -u origin master
 
 pause
