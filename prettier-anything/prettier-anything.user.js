0a27cd2e80d5aafa7b8fe07e563dd3c8da251b99
Adrien Pyke <adpyke@gmail.com>
2019-10-09 17:18:32 -0400

    removed semis

--- prettier-anything/prettier-anything.user.js
+++ prettier-anything/prettier-anything.user.js
@@ -10,12 +10,12 @@
 // ==/UserScript==
 /* global prettier prettierPlugins GM_setClipboard, GM_xmlhttpRequest */
 
-'use strict';
+'use strict'
 
 const deps = [
   'https://unpkg.com/prettier/standalone.js',
   'https://unpkg.com/prettier/parser-babylon.js'
-];
+]
 
 const loadDep = url =>
   new Promise((resolve, reject) => {
@@ -24,81 +24,79 @@ const loadDep = url =>
       url,
       onload: res => resolve(res.responseText),
       onerror: () => reject(new Error(`Failed to load ${url}`))
-    });
-  });
+    })
+  })
 
 const config = {
   parser: 'babel',
   printWidth: 100,
   semi: false,
   singleQuote: true
-};
+}
 
-const p = (...args) => (console.log(...args), args[0]);
+const p = (...args) => (console.log(...args), args[0])
 
-let loaded = false;
+let loaded = false
 const load = () => {
-  if (loaded) return;
-  loaded = true;
-  return Promise.all(deps.map(loadDep)).then(scripts => scripts.map(eval)); // eslint-disable-line no-eval
-};
+  if (loaded) return
+  loaded = true
+  return Promise.all(deps.map(loadDep)).then(scripts => scripts.map(eval)) // eslint-disable-line no-eval
+}
 
 const getSelection = () => {
-  const elem = document.activeElement;
+  const elem = document.activeElement
   if (['INPUT', 'TEXTAREA'].includes(elem.nodeName)) {
-    const { selectionStart: start, selectionEnd: end } = elem;
-    return elem.value.slice(start, end);
+    const { selectionStart: start, selectionEnd: end } = elem
+    return elem.value.slice(start, end)
   } else if (elem.contentEditable) {
-    if (!document.getSelection().toString()) return;
-    document.execCommand('copy');
-    return navigator.clipboard.readText();
+    if (!document.getSelection().toString()) return
+    document.execCommand('copy')
+    return navigator.clipboard.readText()
   } else {
-    return document.getSelection().toString();
+    return document.getSelection().toString()
   }
-};
+}
 
 const insertText = text => {
-  const elem = document.activeElement;
-  if (
-    ['INPUT', 'TEXTAREA'].includes(elem.nodeName) &&
-    typeof InstallTrigger !== 'undefined'
-  ) {
-    const { selectionStart: start, selectionEnd: end } = elem;
-    elem.value = elem.value.slice(0, start) + text + elem.value.slice(end);
+  const elem = document.activeElement
+  if (['INPUT', 'TEXTAREA'].includes(elem.nodeName) && typeof InstallTrigger !== 'undefined') {
+    console.log('blahhh')
+    const { selectionStart: start, selectionEnd: end } = elem
+    elem.value = elem.value.slice(0, start) + text + elem.value.slice(end)
   } else {
-    document.execCommand('insertText', false, text);
+    document.execCommand('insertText', false, text)
   }
-};
+}
 
 window.addEventListener('keydown', e => {
-  console.log(e);
+  console.log(e)
   if (e.altKey && e.shiftKey && e.key.toUpperCase() === 'I') {
-    (async () => {
-      const code = await getSelection();
-      const clip = e.ctrlKey;
-      p('key combo HIT, selection = ', code, '; clip = ', clip);
-      if (!code) return p('no selection, so nothing to do');
-      e.preventDefault();
-      p('--- PRETTIER START ---');
-      p('Loading Prettier');
-      const loadStart = Date.now();
+    ;(async () => {
+      const code = await getSelection()
+      const clip = e.ctrlKey
+      p('key combo HIT, selection = ', code, '; clip = ', clip)
+      if (!code) return p('no selection, so nothing to do')
+      e.preventDefault()
+      p('--- PRETTIER START ---')
+      p('Loading Prettier')
+      const loadStart = Date.now()
 
-      await load();
-      p('Loaded, delta = ', Date.now() - loadStart);
+      await load()
+      p('Loaded, delta = ', Date.now() - loadStart)
       const formatted = prettier.format(code, {
         ...config,
         plugins: prettierPlugins
-      });
+      })
       if (clip) {
-        GM_setClipboard(formatted);
-        document.getSelection().empty();
+        GM_setClipboard(formatted)
+        document.getSelection().empty()
       } else {
-        insertText(formatted);
+        insertText(formatted)
       }
-      p('BEFORE:\n', code);
-      p('AFTER:\n', formatted);
-      p('--- PRETTIER END ---');
-    })();
-    return false;
+      p('BEFORE:\n', code)
+      p('AFTER:\n', formatted)
+      p('--- PRETTIER END ---')
+    })()
+    return false
   }
-});
+})
