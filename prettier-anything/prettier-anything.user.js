9b408ff7d4769e6c3c5e4a1439c536802c4d9a3f
Adrien Pyke <adpyke@gmail.com>
2019-10-10 18:54:26 -0400

    update gm config

--- prettier-anything/prettier-anything.user.js
+++ prettier-anything/prettier-anything.user.js
@@ -10,7 +10,7 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_registerMenuCommand
-// @require      https://gitcdn.link/repo/kufii/My-UserScripts/c9bef4a6732c00b5f53896a6c86eab812f75ce86/libs/gm_config.js
+// @require      https://gitcdn.link/repo/kufii/My-UserScripts/b06cf8745c97944945b646fde8a44888a939110e/libs/gm_config.js
 // ==/UserScript==
 /* global prettier prettierPlugins GM_setClipboard GM_xmlhttpRequest GM_registerMenuCommand GM_config */
 
