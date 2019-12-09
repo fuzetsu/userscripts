23c504c808fe73806518e6f9b64852fd5c240add
68681395 <happycoder@foxmail.com>
2019-12-09 15:14:19 +0800

    delete test file

--- hunttingteacheron51talk/hunttingteacher.test.es6
+++
@@ -1,7 +0,0 @@
-let a = 10
-let b = (m) => {
-  if (m <2)
-    return 1;
-  return m * b(m - 1);
-};
-console.log(b());
--- hunttingteacheron51talk/hunttingteacher.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var a = 10;
-var b = function b(m) {
-  if (m < 2) return 1;
-  return m * b(m - 1);
-};
-console.log(b());
-//# sourceMappingURL=hunttingteacher.test.js.map
