22c605543bc249434661d3d58698210d22ea3f94
68681395 <happycoder@foxmail.com>
2020-04-03 17:39:37 +0800

    update version for finallizion

--- hunttingteacheron51talk/hunttingteacher.user.js
+++ hunttingteacheron51talk/hunttingteacher.user.js
@@ -4,7 +4,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 // ==UserScript==
 // @name         51talk选择最好最合适的老师-经验-好评率-年龄-收藏数
-// @version      2020.4.3001
+// @version      2020.4.3002
 // @namespace    https://github.com/niubilityfrontend
 // @description  辅助选老师-排序显示，经验值计算|好评率|显示年龄|列表显示所有教师
 // @author       jimbo
