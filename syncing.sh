1bc9a09c2ce9885725542b84dc9ac4b2004e9c7c
68681395 <happycoder@foxmail.com>
2020-04-22 10:22:11 +0800

    Just auto-commit for synchronizing workspace

--- syncing.sh
+++ syncing.sh
@@ -2,7 +2,7 @@ echo -n "输入提交介绍，默认为‘auto commit’ ->"
 read msg
  if [ -z "$msg" ]; then
      echo "committing as comment 'Just auto-commit for synchronizing workspace'"
-	 msg="auto commit"
+	 msg="Just auto-commit for synchronizing workspace"
  fi
 git pull
 git add *
